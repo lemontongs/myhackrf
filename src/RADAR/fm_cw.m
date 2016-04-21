e7e8320cee1ee5e90fcb04094c524bdbe372d6dd
Matt Lamontagne <lemontongs@gmail.com>
2016-04-20 23:56:36 -0400

    rti working, mti filter

--- src/RADAR/fm_cw.m
+++ src/RADAR/fm_cw.m
@@ -1,9 +1,12 @@
 
 clear; close all; clc
 
-%% Read from file
+%% Collect data
 
-!hackrf_transfer -r rx.dat -d 22be1 -f 2480000000 -a 1 -l 40 -g 0 -s 8000000 -n 4000000
+!hackrf_transfer -r rx.dat -d 22be1 -f 2490000000 -a 1 -l 40 -g 0 -s 8000000 -n 8000000
+
+%% Process
+
 filename = 'rx.dat';
 fd = fopen(filename,'r');
 data = fread(fd, 'int8');
@@ -11,14 +14,12 @@ fclose(fd);
 data = reshape(data, 2, []);
 data = complex(data(1,:), data(2,:));
 
-%% Process
 
 pri = 0.001;
 prf = 1/pri;
 fs = 8e6;
-pulse_offset = 0;
 pw = 1e-3;
-df = 1.5e6;
+df = 1.4998e6;
 chirp_width = 200e3;
 
 
@@ -34,51 +35,78 @@ win = kaiser(N+1, Beta);
 b  = fir1(N, Fc/(fs/2), 'low', win, flag);
 t = 0:1/fs:length(data)/fs; t(end) = [];
 
-data = data.*exp(j*2*pi*(-1.6e6)*t); % shifted version
+data = data - mean(data);
+data = data.*exp(1i*2*pi*(-(df+(chirp_width/2)))*t); % shifted version
 data = filtfilt(b,1, data);
 
-figure(100); plot(linspace(-fs/2,fs/2,length(data)), 10*log10(fftshift(abs(fft(data)))));
-figure(101); plot(t,real(data));
+data = data(8000*10:end); % Remove initial charge up
+t = t(1:length(data));
+
+% figure(100); plot(linspace(-fs/2,fs/2,length(data)), 10*log10(fftshift(abs(fft(data)))));
+% figure(101); plot(t,real(data));
 
 % match filter
 slopeFactor = (chirp_width)/(2 * pw);
 t2 = 0:1/fs:pri-(1/fs);
-% match_signal = 100 * exp(j * 2.0 * pi * ((df*t)+(slopeFactor*(t.^2))));
-match_signal=  100 * exp(j * 2   * pi * ( t2*(-chirp_width/2) + slopeFactor*t2.^2));
+match_signal = exp(1i * 2.0 * pi * (((-chirp_width/2)*t2)+(slopeFactor*(t2.^2))));
+% match_signal=  100 * exp(1i * 2   * pi * ( t2*(-chirp_width/2) + slopeFactor*t2.^2));
 
 % data = data(3799198:end);
-corrResult = xcorr(data,match_signal);
-corrResult(1:length(data)-1) = [];
+% corrResult = xcorr(data,match_signal);
+% corrResult(1:length(data)-1) = [];
 
-figure(102); plot(linspace(-fs/2,fs/2,length(corrResult)), 10*log10(fftshift(abs(fft(corrResult)))));
-figure(103); plot(10*log10(abs(corrResult)));
+DATA = fft(data); 
+MATCH_SIGNAL = fft(match_signal, length(DATA));
+CORR_RESULT = DATA.*conj(MATCH_SIGNAL);
+corrResult = ifft(CORR_RESULT);
+% figure; plot(10*log10(abs(ifft(CORR_RESULT))))
 
 [pks,locs] = findpeaks(abs(corrResult), 'MINPEAKDISTANCE', 7500);
 
+
+% figure(102); plot(linspace(-fs/2,fs/2,length(corrResult)), 10*log10(fftshift(abs(fft(corrResult)))));
+
+
 pulses = [];
 nsamples = pri*fs;
-for k =1:length(locs)-1
-    pulses(k,:) = data(locs(k):locs(k)+nsamples-1);
+for k = 1:length(locs)-3
+    pulses(k,:) = data(locs(k):locs(k)+nsamples-1) - mean(data(locs(k):locs(k)+nsamples-1));
 end
 
-num_pulses = size(pulses,1);
+num_pulses = size(pulses,1)-1;
 pulse_size = size(pulses,2);
 
+y = zeros(num_pulses, pulse_size);
+for k = 1:num_pulses
+%     y(k,:) = pulses(k,:) - 2*pulses(k+1,:) + pulses(k+2,:); % -2 from num_pulse
+    y(k,:) = pulses(k,:) - pulses(k+1,:);
+end
+
+
+% Compute range time intensity
+rti = zeros(pulse_size,num_pulses);
+for ii = 1:num_pulses
+    tmp = abs(xcorr(y(ii,:), match_signal));
+    rti(:,ii) = tmp(pulse_size:end);
+end
+
+
 % Compute range doppler map
-rdm = zeros(size(pulses));
+rdm = zeros(size(y));
 for ii = 1:pulse_size
-    rdm(:,ii) = fftshift(abs(fft(pulses(:,ii))));
+    rdm(:,ii) = fftshift(abs(fft(y(:,ii))));
 end
 
 %
 % r = tc/2
 %
-range = ((pulse_offset:size(pulses,2)-1+pulse_offset) .* (1/fs)) .* 3e8 ./ 2;
+range = ((0:size(pulses,2)-1) .* (1/fs)) .* 3e8 ./ 2;
 
 figure(10);
 
 subplot(221);
-imagesc(range, 1:num_pulses, abs(pulses'));
+imagesc(1:num_pulses, range, abs(y'));
+set(gca,'YDir','normal')
 title('Pulse Amplitude')
 xlabel('Pulse Number')
 ylabel('Range (m)')
@@ -93,8 +121,14 @@ ylabel('Range (m)')
 colorbar;
 
 subplot(223);
-plot(range, sum(abs(pulses))./num_pulses, '.-');
-title('Mean Amplitude')
+imagesc(range, 1:num_pulses, 20*log10(rti));
+title('Range Time Intensity')
 xlabel('Range (m)')
-ylabel('Amplitude (counts)')
+ylabel('Pulse Number')
 
+
+subplot(224);
+plot(10*log10(abs(corrResult)));
+hold on
+plot(locs,10*log10(pks), 'r*');
+title('Match Filter Response')
--- src/RADAR/main_tx.cpp
+++ src/RADAR/main_tx.cpp
@@ -17,8 +17,8 @@ HackRFDevice hackrf;
 uint64_t fc_hz      = 2490e6; // center freq
 double   fs_hz      = 8e6;   // sample rate
 uint32_t lna_gain   = 0;
-uint8_t  amp_enable = 0;
-uint32_t txvga_gain = 0;
+uint8_t  amp_enable = 1;
+uint32_t txvga_gain = 10;
 
 
 void signal_handler(int s)
@@ -33,7 +33,27 @@ void signal_handler(int s)
 // Callback function for tx samples
 //
 //262144
-double t = 0.0;
+
+std::size_t waveform_index = 0;
+std::vector<uint8_t> waveform_i;
+std::vector<uint8_t> waveform_q;
+
+int sample_block_cb_fn(hackrf_transfer* transfer)
+{
+    for (int ii = 0; ii < transfer->valid_length; ii+=2)
+    {
+        transfer->buffer[ii+0] = waveform_i[waveform_index];
+        transfer->buffer[ii+1] = waveform_q[waveform_index];
+        waveform_index++;
+        if ( waveform_index > waveform_i.size() )
+            waveform_index = 0;
+
+    }
+    
+    return 0;
+}
+
+
 double dt = 1.0/fs_hz;
 double df = 1.5e6; // 1MHz baseband CW
 double pri = 1e-3;
@@ -42,78 +62,41 @@ double amp = 100;
 double chirp_width = df + 200e3;
 double slopeFactor = (chirp_width - df)/(2.0*pw);
 
-
+void createWaveform()
+{
+    
 #define SAVE_FILE
 #ifdef SAVE_FILE
 int write_file = 1;
 std::ofstream ofile("tx.csv", std::ofstream::out);
 #endif
 
-int sample_block_cb_fn(hackrf_transfer* transfer)
-{
-    for (int ii = 0; ii < transfer->valid_length; ii+=2)
+    double t = 0.0;
+    while ( t <= pw )
     {
-        double i = 0.0;
-        double q = 0.0;
-        if ( t < pw ) // 40 ns (1 samples)
-        {
-            
-            
-            i = amp * cos( 2.0 * PI * ((df*t)+(slopeFactor*pow(t,2))) );  // I
-            q = amp * sin( 2.0 * PI * ((df*t)+(slopeFactor*pow(t,2))) );  // Q
-            /*
-            if ( t > pw / 2.0 )
-                dftemp = df * 2.0;
-            else
-                dftemp = df;
-            
-            i = amp * cos( 2.0 * PI * (0.0) * t );  // I
-            q = amp * sin( 2.0 * PI * (0.0) * t );  // Q
-            */
-            uint8_t i8 = uint8_t(i);
-            uint8_t q8 = uint8_t(q);
-            
-            transfer->buffer[ii+0] = i8;
-            transfer->buffer[ii+1] = q8;
-            
-            //transfer->buffer[ii+0] = amp;
-            //transfer->buffer[ii+1] = amp;
-        }
-        else
-        {
-            transfer->buffer[ii+0] = 0;
-            transfer->buffer[ii+1] = 0;
-        }
+        double i = amp * cos( 2.0 * PI * ((df*t)+(slopeFactor*pow(t,2))) );  // I
+        double q = amp * sin( 2.0 * PI * ((df*t)+(slopeFactor*pow(t,2))) );  // Q
+
+        uint8_t i8 = uint8_t(i);
+        uint8_t q8 = uint8_t(q);
+
+        waveform_i.push_back(i8);
+        waveform_q.push_back(q8);
         
 #ifdef SAVE_FILE
-        if ( write_file == 1 )
-        {
-            ofile << t << ","
+        ofile << t << ","
 //                  << unsigned(transfer->buffer[ii+1]) << ","
 //                  << unsigned(transfer->buffer[ii+1]) << std::endl;
-                  << i << ","
-                  << q << std::endl;
-        }
+        << i << ","
+        << q << std::endl;
 #endif
-        
+
         t = t+dt;
-        if ( t >= pri )
-        {
-            t = 0.0;
-        }
     }
-    
-    
 
 #ifdef SAVE_FILE
-    if ( write_file == 1 )
-    {
-        write_file = 0;
-        ofile.close();
-    }
+    ofile.close();
 #endif
-    
-    return 0;
 }
 
 
@@ -128,7 +111,8 @@ int main()
     sigemptyset(&sigIntHandler.sa_mask);
     sigIntHandler.sa_flags = 0;
     sigaction(SIGINT, &sigIntHandler, NULL);
-
+    
+    createWaveform();
     
     if ( ! hackrf.initialize( "23ec7" ) )
     {
